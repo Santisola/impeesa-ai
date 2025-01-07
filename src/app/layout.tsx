61712c355f5a82d596a5df12f572c8133c6bfa9e
Santiago Isola <santiago.i@knownonline.com>
2025-01-07 10:36:51 -0300

    Replace vh for dvh, fix horizontal scroll on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`antialiased min-h-screen`}
+        className={`antialiased min-h-[100dvh]`}
       >
         <Header />
         {children}
